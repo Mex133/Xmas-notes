f4201d1c064607f84c257bdb740ac962811f1a61
Mex133 <renaultsafran@mail.ru>
2022-12-26 11:25:15 +0300

    Xmas

---
+++ README.md
@@ -0,0 +1,3 @@
+Funny Christmas ReactJS ToDo application :3
+
+![preview]{img/preview.png}
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+const Header = () => {
+  return (<div className='header'>
+    <div className='header-menu'>
+    <b class="tittle"> Christmas to-do </b>
+      <nav className='header-nav' />
+    </div></div>)
+}
+
+export default Header
